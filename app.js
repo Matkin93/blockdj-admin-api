922ff81a6c31690813b6331b60772b70b9690cbf
Robert Coster <rob@signalfire.co.uk>
2018-10-02 06:47:30 +0100

    Basic express setup

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const apiRouter = require('./routers/api');
+const bodyParser = require('body-parser');
+const {DB_URL = require('./config').DB_URL} = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+mongoose.connect(DB_URL, {useNewUrlParser: true})
+    .then(() => {
+        console.log(`Connected to Mongo via ${DB_URL}...`);
+    });
+
+app.use('/api', apiRouter);
+
+app.use('/*', (req, res, next) => {
+    next({msg: 'Page not found', status: 404});
+});
+
+app.use(({msg, status}, req, res, next) => {
+    if (status) res.status(status).send({status, msg});
+    else res.status(500).send({msg:'Internal server error', status: 500});
+});
+
+module.exports = app;
