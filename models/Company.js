ea0e1318321f8e37be49b5c6b3a926657c2c90c9
Robert Coster <rob@signalfire.co.uk>
2018-10-02 15:59:33 +0100

    Changed gitignore, added Company model

---
+++ models/Company.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const CompanySchema = new Schema({
+  user_id: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  facebook_url: {
+    type: String
+  },
+  instagram: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date
+  }
+})
+
+module.exports = mongoose.model('companies', CompanySchema);
