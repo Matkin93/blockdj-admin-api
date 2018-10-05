8ae45bec7bddf2c998509f77b40f9cc72ecd8d9f
Robert Coster <rob@signalfire.co.uk>
2018-10-05 13:09:00 +0100

    Early morning work, sorry forgot

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,1 +1,17 @@
-// Tests here
+const app = require('../app');
+const mongoose = require('mongoose');
+const { expect } = require('chai');
+const request = require('supertest')(app);
+
+describe('BlockDJ Admin API', () => {
+    describe('wibble', () => {
+        it('GET homepage', () => {
+            return request
+                .get('/api/cities')
+                .expect(200)
+                .then(res => {
+                    console.log(res.body);
+                })
+        })
+    });
+}); 
