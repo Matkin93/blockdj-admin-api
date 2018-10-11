b1015fa5b1cb2d5b46494c6cf9aea3743345ec08
andy hunt <andyhunt200@hotmail.com>
2018-10-11 09:27:50 +0100

    Minor changes for config for heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const {PORT = require('./config').PORT} = process.env;
+const { PORT = require('./config').PORT } = process.env;
+const port = process.env.PORT || 9090
+
 
 const app = require('./app');
 
-app.listen(PORT, err => {
+app.listen(port, err => {
     if (err) console.log(err);
     else console.log(`Server listening on port ${PORT}...`);
 })
